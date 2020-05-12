b7da4edf8e654c36e3a520ad900f2584e44eaa99
Egbayelo Oluwatobi <tobi.egbayelo@termii.com>
2020-05-11 18:25:11 -0700

    changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,106 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        Termii
-      </h1>
-      <h2 class="subtitle">
-        Termii helps African businesses use sms, email, voice and instant messaging channels to verify and communicate with customers
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+<div>
+  <div class="pace  pace-inactive"><div class="pace-progress" data-progress-text="100%" data-progress="99" style="transform: translate3d(100%, 0px, 0px);">
+    <div class="pace-progress-inner"></div>
+  </div>
+    <div class="pace-activity"></div></div>
+
+
+  <!-- Main navbar -->
+  <div class="navbar navbar-verse navbar-fixed-top">
+    <div class="navbar-header" style="background: #fff !important;">
+      <a href="http://34.220.128.230/dashboard">
+        <img class="margin-lx" src="http://34.220.128.230/assets/images/logo.png" alt="logo" data-src="http://34.220.128.230/assets/images/logo.png" data-src-retina="http://34.220.128.230/assets/images/logo.png" width="155px" height="auto">
+      </a>
+
+      <ul class="nav navbar-nav pull-right visible-xs-block">
+        <li>
+          <a class="mobile-menu-button z-in" data-toggle="collapse" data-target="#navbar-mobile">
+            <i class="icon-menu7 blue-t"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="navbar-collapse collapse" id="navbar-mobile">
+
+      <ul class="nav navbar-nav navbar-right z-in">
+
+        <ul class="nav navbar-nav">
+          <div class="drops">
+            <a class="men">Why Termii?</a>
+            <div class="dropdown-content menu-t">
+              <a href="http://34.220.128.230/about" class="pad-width">
+                About Us
+              </a>
+              <a href="http://34.220.128.230/usecases" class="pad-width">
+                Usecase
+              </a>
+              <a href="http://34.220.128.230/investors" class="up-top pad-width">
+                Milestones
+              </a>
+            </div>
+          </div></ul>
+
+        <ul class="nav navbar-nav">
+          <div class="drops">
+            <a class="men">Products</a>
+            <div class="dropdown-content menu-t">
+              <a href="http://34.220.128.230/verify-app" class="pad-width" style="width: 270px;"><small class="blue-t">TOKEN</small><br>OTP Verification</a>
+              <a href="http://34.220.128.230/messaging-app" class="pad-width" style="width: 270px;"><small class="blue-t">SWITCH</small><br>Cross-channel Messaging</a>
+              <a href="http://34.220.128.230/analytics-app" class="pad-width" style="width: 270px;"><small class="blue-t">INSIGHT</small><br>Customer Status Analytics</a>
+              <a href="http://34.220.128.230/guideline" class="up-top pad-width" style="width: 270px;"><small class="blue-t">DEVELOPERS</small><br>Full API Documentation</a>
+            </div>
+          </div></ul>
+
+        <li>
+          <a href="http://34.220.128.230/plans" class="pad-width">
+            Pricing
+          </a>
+        </li>
+
+        <li>
+          <a class="pad-width">
+            <i class="entypo-phone"></i> 234-9022557951
+          </a>
+        </li>
+
+
+        <li>
+          <a class="btn btx-login" href="http://34.220.128.230/login">Login
+          </a>
+        </li>
+        <li>
+          <a class="btn bt-login btn-icon" href="http://34.220.128.230/register"> Signup<i class="fa fa-angle-double-right ico-r"></i></a>
+        </li>
+      </ul>
     </div>
   </div>
+  <!-- /main navbar -->
+
+</div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+
 
 export default {
-  components: {
-    Logo
+  head: {
+    script: [
+      { }
+    ],
+    link: [
+      { rel: 'stylesheet',  href: 'http://34.220.128.230/assets/css/bootstrap.css?v=9', type: "text/css" },
+      { rel: 'stylesheet',  href: 'http://34.220.128.230/assets/css/core.css?v=11', type: "text/css" },
+      { rel: 'stylesheet',  href: 'http://34.220.128.230/css/app.css?v=26', type: "text/css" },
+      { rel: 'stylesheet',  href: 'http://34.220.128.230/css/theme.css?v=9', type: "text/css" },
+    ]
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+
 
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
