38d3b11cdf530399b5ebcc4f0000377b13c76c10
Egbayelo Oluwatobi <tobi.egbayelo@termii.com>
2020-07-11 18:22:46 -0700

    removed input box shadow

--- pages/set-unit-limit.vue
+++ pages/set-unit-limit.vue
@@ -192,8 +192,6 @@
     background-image: none;
     border: 1px solid #ddd;
     border-radius: 3px;
-    -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
-    box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
     -webkit-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
     -o-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
     transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
