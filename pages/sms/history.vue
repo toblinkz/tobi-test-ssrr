be62f3ed79fb2f963cd52bc2db7b8e03c937b5d3
Egbayelo Oluwatobi <tobi.egbayelo@termii.com>
2020-07-15 19:15:07 -0700

    add style to hide side bar for mobile view

--- pages/sms/history.vue
+++ pages/sms/history.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-fluid body">
     <div id="msb" class="col-md-2">
-      <Sidebar></Sidebar>
+      <Sidebar class="hidden-xs"></Sidebar>
     </div>
     <div class="col-md-10">
       <DashboardNavbar></DashboardNavbar>
@@ -56,7 +56,7 @@
                             </div>
                           </div>
                           <div class="col-lg-6 mb-20">
-                              <DashboardChart ></DashboardChart>
+                              <DashboardChart class="hidden-xs"></DashboardChart>
                           </div>
 
                         </div>
