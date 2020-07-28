ac04b198c3edf1a66054c18b8281421170b4e50a
Egbayelo Oluwatobi <tobi.egbayelo@termii.com>
2020-07-28 08:58:45 -0700

    created ButtonSpinner component

---
+++ components/general/ButtonSpinner.vue
@@ -0,0 +1,65 @@
+<template>
+  <a>
+    <button type="submit"  class="btnl bg-blue m-t-10" :disabled="isDisabled">
+      {{button_text}}
+      <span v-show="isLoading">
+        <img src="/images/spinner.svg" height="20px" width="30px"/>
+      </span>
+   </button>
+  </a>
+</template>
+
+<script>
+    export default {
+        name: "ButtonSpinner",
+      props: {
+          isDisabled: {
+            type: Object,
+            required: true
+          },
+        button_text: {
+            type: String,
+          required: true
+        },
+        isLoading: {
+            type: Boolean
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  @import "assets/css/general_style/spinner.css";
+  .bg-blue {
+    background: linear-gradient(-48deg, #0DCBE5 -30%, #365899 60%);
+    display: inline-block !important;
+    color: #fff !important;
+    border-radius: 4px;
+    vertical-align: bottom !important;
+    position: relative;
+    border: transparent;
+    box-shadow: 8px 10px 20px 0 rgba(0, 0, 0, 0.22);
+    transition: .35s;
+    padding: 7px 15px 8px 15px !important;
+    font-weight: 600 !important;
+    font-size: 13px !important;
+  }
+  button:focus, button:active{
+    outline: none;
+  }
+  button[disabled] {
+    opacity: .5;
+    pointer-events: none;
+  }
+  .btnl{
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+    transition: color 0.1s linear 0s, background-color 0.1s linear 0s, opacity 0.2s linear 0s !important;
+  }
+
+  .btnl:hover {
+    background-color: #fafafa;
+    border: 1px solid rgba(98, 98, 98, 0.27);
+  }
+</style>
