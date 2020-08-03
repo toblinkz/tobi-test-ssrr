acca6717e82396462de42052f566efa68c7b793b
Egbayelo Oluwatobi <tobi.egbayelo@termii.com>
2020-08-02 20:06:56 -0700

    store bearer token in vuex store

--- store/index.js
+++ store/index.js
@@ -8,20 +8,28 @@ export const getters = {
   },
   isRegistered(state){
     return state.user_registered;
+  },
+  getBearerToken(state){
+    return state.bearer_token;
   }
 }
 export const state = () => ({
 
-  user_registered: null
+  user_registered: null,
+  bearer_token: localStorage.getItem('auth._token.local').slice(7) || ''
+
 });
 
 export const mutations = {
   changeRegisteredState (state) {
     if (state.user_registered === null){
       state.user_registered = true;
-    } else {
+    } else if(state.user_registered === true){
       state.user_registered = null;
     }
-
+  },
+  setBearerToken(state, bearer_token){
+    state.bearer_token = bearer_token;
   }
+
 }
