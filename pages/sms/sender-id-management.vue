90cc116c0dc86bcb1cac7ee73a987ca1ced70700
Egbayelo Oluwatobi <tobi.egbayelo@termii.com>
2020-08-02 20:16:39 -0700

    dynamically render sender id in the table

--- pages/sms/sender-id-management.vue
+++ pages/sms/sender-id-management.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-fluid body">
+  <div class="container-fluid ">
     <div id="msb" class="col-md-2 hidden-xs">
       <Sidebar></Sidebar>
     </div>
@@ -31,7 +31,7 @@
                             <center>
                               <!-- START PANEL -->
                               <div class="panel-transparent">
-                                <p id="welcome" style="margin-top: 10px;margin-bottom: 15px"><i class="entypo-list-add"></i> Register your device ID!</p>
+                                <p id="welcome" style="margin-top: 10px;margin-bottom: 15px"><i class="entypo-list-add"></i> Register your sender ID!</p>
                                 <p class="insight hidden-xs">Approve your sender ID to send sms to push out messages to your customers.<br>Approved device ID's are required to send messages on Termii!</p>
 
                                 <div class="row">
@@ -40,7 +40,7 @@
                                     </div>
                                     <div class="col-sm-4">
                                       <br>
-                                      <a class="btn btn-primary" @click="showModal = true"><i class="fa fa-plus"></i> Make a new request</a>
+                                      <a class="btn btn-primary" @click="show"><i class="fa fa-plus"></i> Make a new request</a>
                                     </div>
                                     <div class="col-sm-4 hidden-xs">
                                     </div>
@@ -73,63 +73,16 @@
                                 </tr>
                                 </thead>
                                 <tbody>
-                                <tr>
-                                  <td data-label="SL" class="hidden-xs">1</td>
-                                  <td data-label="Sender ID"><p>Termii</p></td>
-                                  <td data-label="Status"><p class="label label-success">Approved</p>
+                                <tr v-for="row in response_data.data" :key="row.id">
+                                  <td data-label="SL" class="hidden-xs">{{row.id}}</td>
+                                  <td data-label="Sender ID"><p>{{row.sender_id}}</p></td>
+                                  <td data-label="Status"><p class="label label-success">{{row.status}}</p>
                                   </td>
-                                  <td data-label="Type"><p></p></td>
-                                  <td data-label="Type"><p></p></td>
+                                  <td data-label="Type"><p>{{row.company}}</p></td>
+                                  <td data-label="Type"><p>{{row.usecase}}</p></td>
 
                                 </tr>
-                                <tr>
-                                  <td data-label="SL" class="hidden-xs">2</td>
-                                  <td data-label="Sender ID"><p>TermiiW</p></td>
-                                  <td data-label="Status"><p class="label label-warning">Pending Approval</p></td>
 
-                                  <td data-label="Type"><p></p></td>
-                                  <td data-label="Type"><p></p></td>
-                                </tr>
-                                <tr>
-                                  <td data-label="SL" class="hidden-xs">2</td>
-                                  <td data-label="Sender ID"><p>TermiiW</p></td>
-                                  <td data-label="Status"><p class="label label-warning">Pending Approval</p></td>
-
-                                  <td data-label="Type"><p></p></td>
-                                  <td data-label="Type"><p></p></td>
-                                </tr>
-                                <tr>
-                                  <td data-label="SL" class="hidden-xs">2</td>
-                                  <td data-label="Sender ID"><p>TermiiW</p></td>
-                                  <td data-label="Status"><p class="label label-warning">Pending Approval</p></td>
-
-                                  <td data-label="Type"><p></p></td>
-                                  <td data-label="Type"><p></p></td>
-                                </tr>
-                                <tr>
-                                  <td data-label="SL" class="hidden-xs">2</td>
-                                  <td data-label="Sender ID"><p>TermiiW</p></td>
-                                  <td data-label="Status"><p class="label label-warning">Pending Approval</p></td>
-
-                                  <td data-label="Type"><p></p></td>
-                                  <td data-label="Type"><p></p></td>
-                                </tr>
-                                <tr>
-                                  <td data-label="SL" class="hidden-xs">2</td>
-                                  <td data-label="Sender ID"><p>TermiiW</p></td>
-                                  <td data-label="Status"><p class="label label-warning">Pending Approval</p></td>
-
-                                  <td data-label="Type"><p></p></td>
-                                  <td data-label="Type"><p></p></td>
-                                </tr>
-                                <tr>
-                                  <td data-label="SL" class="hidden-xs">2</td>
-                                  <td data-label="Sender ID"><p>TermiiW</p></td>
-                                  <td data-label="Status"><p class="label label-warning">Pending Approval</p></td>
-
-                                  <td data-label="Type"><p></p></td>
-                                  <td data-label="Type"><p></p></td>
-                                </tr>
                                 </tbody>
                               </table>
                             </div>
@@ -143,9 +96,9 @@
             </main>
           </div>
         </div>
-        <SenderIdModal v-if="showModal" @close="closeModal"></SenderIdModal>
       </div>
     </div>
+    <SenderIdModal  @close="closeModal" @requested="requested"></SenderIdModal>
   </div>
 </template>
 
@@ -153,19 +106,45 @@
     import Sidebar from "../../components/general/Sidebar";
     import DashboardNavbar from "../../components/general/navbar/DashboardNavbar";
    import SenderIdModal from "../../components/modals/SenderIdModal";
+    import {mapGetters} from "vuex";
     export default {
         name: "sender-id-management",
+        middleware:'auth',
       components: {SenderIdModal, DashboardNavbar, Sidebar},
       data(){
           return{
-            showModal: false
+
+            response_data: []
           }
+      },
+      computed: {
+        ...mapGetters(['getBearerToken'])
+
       },
       methods: {
           closeModal(){
             this.showModal = false;
+          },
+        async loadSenderId() {
+          try {
+            let data = await this.$axios.$get('sms/sender-id', {headers: {'Authorization': 'Bearer ' + this.getBearerToken}});
+            this.response_data = data;
+          } catch (e) {
+
           }
-      }
+        },
+        show () {
+          this.$modal.show('sender-id-modal');
+          $("body").css("overflow", "hidden");
+        },
+        requested(){
+            this.loadSenderId();
+          $("body").css("overflow", "auto");
+        }
+      },
+     mounted() {
+       this.loadSenderId();
+     }
     }
 </script>
 
