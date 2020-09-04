d674bde689200a52757a29d8d5b22f3a783e23c5
Egbayelo Oluwatobi <tobi.egbayelo@termii.com>
2020-09-04 14:35:15 +0100

    added headway changelog

--- components/general/navbar/DashboardNavbar.vue
+++ components/general/navbar/DashboardNavbar.vue
@@ -21,12 +21,20 @@
       </script>
 
       <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+					<script>
+						// @see https://docs.headwayapp.co/widget for more configuration options.
+						var HW_config = {
+							selector: "#headway", // CSS selector where to inject the badge
+							account:  "J1d207"
+						}
+					</script>
+					<script async src="https://cdn.headwayapp.co/widget.js"></script>
     </div>
       <div class="navbar-collapse collapse " id="navbar-mobile">
 
-        <ul class="nav navbar-nav navbar-right mt-5">
-          <li class="icon-new hidden-xs" style="color: #fff !important;">
-            <div data-toggle="popover" title="Change log" style="float: left;top: -2px;font-size: 20px;border-width: 0 1px;position: relative;height: 40px;line-height: 42px;cursor: pointer;color: #2c2c2c !important;">
+        <ul class=" nav navbar-nav navbar-right mt-5">
+          <li class="headway hidden-xs" style="color: #fff !important;" id="headway">
+            <div title="Change log" style="float: left;top: -2px;font-size: 20px;border-width: 0 1px;position: relative;height: 40px;line-height: 42px;cursor: pointer;color: #2c2c2c !important;">
               <i class="entypo-bell"></i></div>
           </li>
         </ul>
@@ -35,7 +43,7 @@
           <li class="m-left hidden-xs">
             <nuxt-link to="/message/select-type" type="button" class="btn stretch-a" style="height: 30px !important;margin-top: 8px !important;padding-bottom: 26px;
     color: #fff !important;padding-top: 7px !important;background: linear-gradient(-48deg, #0DCBE5 -30%, #365899 60%) !important;box-shadow: 8px 10px 20px 0 rgba(0, 0, 0, 0.22);font-weight: 100 !important;">
-              <i class="entypo-paper-plane" style="font-size: 14px;"></i> Compose message
+              <i class="entypo-paper-plane " style="font-size: 14px;"></i> Compose message
             </nuxt-link>
           </li>
           <Dropdown id="mobile-menu" class="hide-menu">
@@ -99,7 +107,9 @@
       }
     },
 
-  }
+
+		}
+
 </script>
 
 
