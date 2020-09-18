c4e69ca55544ce2cf5951d99bba3080e4604b089
Egbayelo Oluwatobi <tobi.egbayelo@termii.com>
2020-09-18 05:00:23 +0100

    configure auth module

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,19 +74,26 @@ export default {
 
   axios: {
     // proxyHeaders: false
-    baseURL: 'http://api.sandbox.termii.com/v1/',
+    baseURL: 'https://api.sandbox.termii.com/v1/',
 
   },
+
   toast: {
     position: 'bottom-center',
     duration: 3000
   },
 
 	auth: {
-
+		localStorage: false,
+		cookie: {
+			prefix: 'auth.',
+			options: {
+				maxAge: 600
+			}
+		},
 		redirect:{
 			login: '/login',
-			home: '/dashboard'
+			home: '/'
 
 		},
 		strategies: {
@@ -94,11 +101,12 @@ export default {
 				endpoints: {
 					login: {url: 'auth/login', method: 'post', propertyName: 'access_token'},
 					user: {url: '/user', method: 'get', propertyName: 'data'},
-					logout:{url:'/logout', method:'post'}
+					logout:{url:'auth/logout', method:'get'}
 
 				}
 			}
-		}
+			},
+		plugins: [{src:'~/plugins/axios.js'}, { src: '~/plugins/auth.js', mode: 'client' }]
 	},
 
 
