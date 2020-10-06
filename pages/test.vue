4b15ac8368c6d4499f676cc203d276749cc5802f
Egbayelo Oluwatobi <tobi.egbayelo@termii.com>
2020-10-06 15:50:24 +0100

    test page

---
+++ pages/test.vue
@@ -0,0 +1,24 @@
+<template>
+		<div>
+			<a class="btn btn-primary" @click="showModal">Show Modal</a>
+			<VerificationModal></VerificationModal>
+		</div>
+</template>
+
+<script>
+import VerificationModal from "~/components/modals/VerificationModal";
+export default {
+	name: "test",
+	components: {VerificationModal},
+	methods: {
+		showModal(){
+			this.$modal.show('verification-id-modal');
+			console.log('show')
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
