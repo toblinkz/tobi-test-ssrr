60fd369bff5d59e9b381fc66c3a4d03bf192eb35
Egbayelo Oluwatobi <tobi.egbayelo@termii.com>
2020-10-08 10:57:15 +0100

    commented on the code

--- nuxt.config.js
+++ nuxt.config.js
@@ -117,6 +117,7 @@ export default {
 			},
 		plugins: ['~/plugins/axios.js', { src: '~/plugins/auth.js', mode: 'client' }]
 	},
+	//added env object in other to access API_BASE_URL variable in .env file
 	env: {
 			apiBaseUrl: process.env.API_BASE_URL
 	},
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,3 +1,4 @@
 export default ({  $axios }) => {
+			// passed apiBaseUrl from env object in nuxt config to axios default base url
 	$axios.defaults.baseURL = process.env.apiBaseUrl;
 }
