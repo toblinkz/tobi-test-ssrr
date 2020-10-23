df19ffe6a3c5f8f90ede0c3c6ff7edfee9dc7252
Egbayelo Oluwatobi <tobi.egbayelo@termii.com>
2020-10-23 17:28:43 +0100

    check for active status id when user is loggedIn

--- middleware/inactive_user.js
+++ middleware/inactive_user.js
@@ -1,5 +1,8 @@
 export default function ({store, redirect}) {
-	if (store.state.auth.user.active_status_id.id ===  6){
-		return redirect('/account/deactivate');
+	if (store.state.auth.loggedIn){
+		if (store.state.auth.user.active_status_id.id ===  6) {
+			return redirect('/account/deactivate');
+		}
 	}
+
 }
