d7bd917a43ad5f9c13cbb683b21bec79795a0c26
Egbayelo Oluwatobi <tobi.egbayelo@termii.com>
2020-10-30 23:17:20 +0100

    handled internet connection error

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 export default async function ({ app ,  redirect, store}) {
 	const {$axios} = app
-
+if (navigator.onLine){
 	try {
 		let response = await $axios.get('user', {
 			headers: {
@@ -8,10 +8,14 @@ export default async function ({ app ,  redirect, store}) {
 			}
 		});
 	}catch (e){
-			if (e.response.data.error === 'Token is Expired' || !localStorage.getItem('local')){
-							store.commit('setLIState', false);
-							redirect('/login');
-			}
+		if (e.response.data.error === 'Token is Expired' || !localStorage.getItem('local')){
+			store.commit('setLIState', false);
+			redirect('/login');
+		}
 	}
+}else {
+	$nuxt.$toast.show("No Internet connection");
+}
+
 }
 
