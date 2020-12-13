7bcc90b68fcacfce4fac7b4147305cb2d8594f47
ayomideawe <awe.ayomide@termii.com>
2020-12-13 01:33:18 +0100

    adjust design for country select on number

--- components/modals/BuyNumberModal.vue
+++ components/modals/BuyNumberModal.vue
@@ -8,45 +8,45 @@
 						<button type="button" class="close" @click="close">×</button>
 						<h4 class="modal-title"><strong># Buy Number</strong></h4>
 					</div>
-
 					<div v-show="show_country_select" class="modal-body">
-						<div class="row">
-					<form @submit.prevent="getAvailableCountryNumber">
-						<div class="form-group">
-						<div class="col-md-7" >
-							<label> Select Country</label>
-							<select name="country" class="form-control" v-model="country">
-								<option value="AT" >Austria ( +43 )</option>
-								<option value="AU">Australia ( +61 )</option>
-								<option value="BE">Belgium ( +32 )</option>
-								<option value="BR">Brazil ( +55 )</option>
-								<option value="CA">Canada ( +1 )</option>
-								<option value="CN">China ( +86 )</option>
-								<option value="CZ">Czechia ( +420 )</option>
-								<option value="DK">Denmark ( +45 )</option>
-								<option value="FR">France ( +33 )</option>
-								<option value="GB">United Kingdom ( +44 )</option>
-								<option value="HR">Croatia ( +385 )</option>
-								<option value="IL">Israel ( +972 )</option>
-								<option value="LT">Lithuania ( +370 )</option>
-								<option value="LV">Latvia ( +371 )</option>
-								<option value="NL">Netherlands ( +31 )</option>
-								<option value="NO">Norway ( +47 )</option>
-								<option value="PL">Poland ( +48 )</option>
-								<option value="RO">Romania ( +40 )</option>
-								<option value="SE">Sweden ( +46 )</option>
-								<option value="US">United States of America ( +1 )</option>
-							</select>
-						</div>
-						<div class="col-md-5">
-							<button type="submit" class="btn btn-primary m-r-5">
-								Proceed
-								<span v-show="isLoading">
-																<img src="/images/spinner.svg" height="20px" width="30px"/>
-														</span>
-							</button>
-						</div>
-						</div>
+							<b>Select the country you want to rent a number from:</b>
+						<br />
+						<br />
+							<form @submit.prevent="getAvailableCountryNumber">
+								<div class="row">
+									<div class="col-md-7" >
+										<select name="country" class="form-control" v-model="country">
+											<option value="AT" >Austria ( +43 )</option>
+											<option value="AU">Australia ( +61 )</option>
+											<option value="BE">Belgium ( +32 )</option>
+											<option value="BR">Brazil ( +55 )</option>
+											<option value="CA">Canada ( +1 )</option>
+											<option value="CN">China ( +86 )</option>
+											<option value="CZ">Czechia ( +420 )</option>
+											<option value="DK">Denmark ( +45 )</option>
+											<option value="FR">France ( +33 )</option>
+											<option value="GB">United Kingdom ( +44 )</option>
+											<option value="HR">Croatia ( +385 )</option>
+											<option value="IL">Israel ( +972 )</option>
+											<option value="LT">Lithuania ( +370 )</option>
+											<option value="LV">Latvia ( +371 )</option>
+											<option value="NL">Netherlands ( +31 )</option>
+											<option value="NO">Norway ( +47 )</option>
+											<option value="PL">Poland ( +48 )</option>
+											<option value="RO">Romania ( +40 )</option>
+											<option value="SE">Sweden ( +46 )</option>
+											<option value="US">United States of America ( +1 )</option>
+										</select>
+									</div>
+									<div class="col-md-5">
+										<button type="submit" class="btn btn-primary m-r-5">
+											Proceed
+											<span v-show="isLoading">
+																			<img src="/images/spinner.svg" height="20px" width="30px"/>
+																	</span>
+										</button>
+									</div>
+								</div>
 					</form>
 						</div>
 				</div>
@@ -114,7 +114,7 @@ export default {
 			show_payment_options : false,
 			show_country_select: true,
 			button_text: 'Get Number',
-			country: '',
+			country: 'US',
 			phone_number: '',
 			currency: '',
 			service_charge: '',
@@ -127,8 +127,8 @@ export default {
 	},
 	methods: {
 		close() {
-			this.show_invoice = true;
-				this.show_country_select = false;
+			this.show_invoice = false;
+				this.show_country_select = true;
 			this.$modal.hide('buy-number-modal');
 		},
 		async pay(){
@@ -367,6 +367,7 @@ table {
 .modal-footer .btn + .btn {
 	margin-left: 5px;
 	/* margin-bottom: 0; */
+}
 
 [class^="icon-"], [class*=" icon-"] {
 	color: #F10000 !important;
@@ -380,5 +381,21 @@ table {
 	outline: none;
 	cursor: pointer;
 }
+.form-control {
+	display: block;
+	width: 100%;
+	height: 36px;
+	padding: 7px 12px;
+	/* font-size: 13px; */
+	line-height: 1.5384616;
+	color: #333333;
+	background-color: #fff;
+	background-image: none;
+	border: 1px solid #ddd;
+	border-radius: 3px;
+	-webkit-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+	-o-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+	transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
 }
+
 </style>
