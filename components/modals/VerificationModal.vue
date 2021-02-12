11f65dbc3ccc930fbc2b5beccd9e2f7964221b47
Egbayelo Oluwatobi <tobi.egbayelo@termii.com>
2021-02-12 16:23:45 +0100

    updated info

--- components/modals/VerificationModal.vue
+++ components/modals/VerificationModal.vue
@@ -9,7 +9,7 @@
 				<div class="flex-item-right modal-body">
 					<div class="p-x-y">
 						<p  class="m-t-80 text-bold">Verify it's you</p>
-						<p>Welcome to your Dashboard. We sent a verification code to your email address. Please enter the code into the text field below</p>
+						<p>Welcome to your Dashboard. We sent a verification code to your email address and phone number. Please enter the code into the text field below</p>
 						<form  method="post"  @submit.prevent="verifyCode" >
 							<div class="mt-20">
 								<div class="form-group">
