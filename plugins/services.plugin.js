131c22f2621121e9a59a87e9b01e86af58b8139b
ayomideawe <awe.ayomide@termii.com>
2021-03-09 16:23:20 +0100

    update comment

--- plugins/services.plugin.js
+++ plugins/services.plugin.js
@@ -2,7 +2,7 @@
 import { BillingService } from '@/services/billing.service'
 
 export default ({ app: { $axios } }, inject) => {
-	// pass $axios as a dependency to the RecommendationsService constructor
+	// pass $axios as a dependency to the BillingService constructor
 	const billing = new BillingService($axios)
 
 	// inject the service, making it available in the context, component, store, etc.
