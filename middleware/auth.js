ed95ad56a1d6328330fbde01d37b0b3671c439c7
Egbayelo Oluwatobi <tobi.egbayelo@termii.com>
2021-06-30 10:55:30 +0100

    fix: initialize toaster

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,11 @@
-export default async function ({  app ,  redirect, store, $nuxt}) {
+export default async function ({  app ,  redirect, store, $toast}) {
 	const {$axios} = app
 if (navigator.onLine) {
 	 if (store.state.L_I !== true){
 	 	 return redirect('/login');
 	 }
 }else {
-	$nuxt.$toast.show("No Internet connection");
+	$toast.show("No Internet connection");
 }
 
 }
