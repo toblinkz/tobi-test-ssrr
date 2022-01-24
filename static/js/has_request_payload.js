b6a669bed6413bab1b4ca75101fc73426e5ae442
toblinkz <tobi.egbayelo@termii.com>
2022-01-24 13:33:04 +0100

    fix: trim some characters in signature

--- static/js/has_request_payload.js
+++ static/js/has_request_payload.js
@@ -2,5 +2,6 @@ import crypto from "crypto";
 
 export function hashRequestPayload(data) {
 	let secretKey = process.env.ADSK;
-	return 	crypto.createHmac('sha512', secretKey).update(JSON.stringify(data).trim()).digest('hex');
+	console.log(JSON.stringify(data).replace(/[\/\\']/g,'').trim());
+	return 	crypto.createHmac('sha512', secretKey).update(JSON.stringify(data).replace(/[\/\\']/g,'').trim()).digest('hex');
 }
