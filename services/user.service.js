28a18eafdc95179385fd1e6190c4bc8100392109
Ayomide Awe <awe.ayomide@termii.com>
2022-02-07 07:19:36 +0100

    push to master

--- services/user.service.js
+++ services/user.service.js
@@ -73,6 +73,7 @@ export class UserService {
 	async getLoggedInUserData () {
 		return await this.$axios.$get('user');
 	}
+
 	async updateCompanyName(company_name){
 		return await this.$axios.$patch('user/company', {
 			company_name: company_name
