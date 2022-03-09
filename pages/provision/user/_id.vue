e1c58eb13c9ec1e74c200cbcb4f191551f30c0ca
toblinkz <tobi.egbayelo@termii.com>
2022-03-09 12:37:55 +0100

    fix: fix validation for password

--- pages/provision/user/_id.vue
+++ pages/provision/user/_id.vue
@@ -113,14 +113,14 @@ export default {
 			}
 		},
 		validatePassword(value) {
-			if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!#@$%^&*])(?=.{8,})/.test(value)) {
+			if ( /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!#@$%^_&*-])(?=.{8,})/.test(value)){
 				this.error_message['password'] = '';
 				this.hasPasswordError = false;
-				return;
 			}
-			this.error_message['password'] = 'Password is too weak';
-			this.hasPasswordError = true;
-
+			else {
+				this.error_message['password'] = 'Password is too weak';
+				this.hasPasswordError = true;
+			}
 		},
 		validateConfirmPassword() {
 			if (this.confirm_password !== this.password) {
