171594d88f7ca83d0db14698075687c4c4c7c30c
toblinkz <tobi.egbayelo@termii.com>
2022-04-27 18:33:43 +0100

    log secretkey

--- static/js/has_request_payload.js
+++ static/js/has_request_payload.js
@@ -2,5 +2,6 @@ import crypto from "crypto";
 
 export function hashRequestPayload(data) {
 	let secretKey = process.env.ADSK;
+	console.log('secr',secretKey);
 	return 	crypto.createHmac('sha512', secretKey).update(JSON.stringify(data).replace(/[\/\\']/g,'').trim()).digest('hex');
 }
