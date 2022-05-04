2d68ab49a20c54ba84e3fadba7b51201bd0c83ed
toblinkz <tobi.egbayelo@termii.com>
2022-05-04 10:31:54 +0100

    changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -110,7 +110,7 @@ export default {
 		}],
 	],
 	helmet: {
-		contentSecurityPolicy: false,
+		frameguard: { action: "deny" },
 	},
 	generate: {
 		fallback: true, // if you want to use '404.html' instead of the default '200.html'
